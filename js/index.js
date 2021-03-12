94aa7107e724edeafa5a9288f70e4359be9aaca6
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-03-12 18:02:13 +0100

    worked on cart ani

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ const cartMenu = document.querySelector(".hor1");
 function addToCart(event) {
     event.preventDefault();
     cart.style.borderRadius = "50%";
-    cart.innerHTML = event.target.value;
+    cart.innerHTML = event.detail;
     cart.style.width = "0px";
 
-    console.log(event)
+    console.log(event.detail)
 
     var rect = cart.getBoundingClientRect();
     cartMenu.appendChild(cart);

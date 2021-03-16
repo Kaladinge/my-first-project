cfc052980c3a73f4c1d13f2d59640f0ab0e4364e
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2021-03-16 21:07:49 +0100

    added functions to cart

--- js/index.js
+++ js/index.js
@@ -2,16 +2,37 @@ const cart = document.querySelectorAll(".cart");
 const quantity = document.querySelector(".quantity");
 const sidebar = document.querySelector(".click-area");
 const cartMenu = document.querySelector(".hor1");
+const cartBox = document.querySelector("#cart-box");
+const cartDetails = document.querySelector(".cart-details");
+const keepShopping = document.querySelector(".keep-shopping");
 
-
+let x = 1;
 function addToCart(event) {
     event.preventDefault()
     quantity.style.display = "block";
     cartMenu.setAttribute('href',"checkout.html");
-    quantity.innerHTML = event.detail;
-    console.log(event.detail)
+
+    console.log(x);
+    quantity.innerHTML = x;
+    console.log(event.detail);
+
+
+    cartBox.style.display = "block";
+    cartDetails.innerHTML += `<div class="product-line">
+                            <img src="images/skyrim-logo.jpg" class="gamepicture-cart" alt="Skyrim logo">
+                            <p class="cart-info">Product</p>
+                            <p class="cart-info">Qty: 1</p>
+                            <p class="cart-info">Prize: xxxx</p>
+                            </div>`
+    x = x + 1;
 }
 
+
+function removeCartDetails() {
+    cartBox.style.display = "none";
+}
+
+keepShopping.addEventListener("click", removeCartDetails);
 sidebar.addEventListener("click", addToCart);
 
 
